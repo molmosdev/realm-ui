329037aefbb5210963ce3f8a573b7fbb00e3fa6a
molmos <molmosdev@gmail.com>
2024-10-25 01:12:46 +0200

    Refactor button component styles and update @realm-ui/angular package version to 0.0.0-beta.0.0.27

--- projects/angular/src/lib/core/components/button/button.component.ts
+++ projects/angular/src/lib/core/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, input, viewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, input, output, viewChild } from '@angular/core';
 import { ButtonTypeEnum } from './enums/button-type.enum';
 import { NgClass, NgStyle } from '@angular/common';
 import { Spinner } from '../spinner/spinner.component';
@@ -13,44 +13,9 @@ import { fadeInOutTrigger } from '../../../shared/animations/animations';
   animations: [fadeInOutTrigger],
 })
 export class Button {
-  /* Inputs */
   type = input<ButtonTypeEnum>(ButtonTypeEnum.Primary);
   loading = input<boolean>(false);
-  button = viewChild<ElementRef>('button');
-
-  /**
-   * Get the padding-inline style
-   *
-   * @returns {object} - The padding-inline style
-   */
-  get paddingInlineStyle(): object {
-    // Get the native element of the button
-    let nativeElement = this.button()?.nativeElement;
-
-    // Get only the number from border-radius
-    let borderRadiusNumber = parseInt(window.getComputedStyle(nativeElement).getPropertyValue('border-radius'), 10);
-
-    // Define the limits for padding-inline
-    let minPadding = 10;
-    let maxPadding = 18;
-
-    // Define the limits for borderRadiusNumber (adjust these values according to your needs)
-    let minBorderRadius = 0;
-    let maxBorderRadius = 50;
-
-    // Calculate the corresponding padding
-    let padding =
-      minPadding +
-      (maxPadding - minPadding) * ((borderRadiusNumber - minBorderRadius) / (maxBorderRadius - minBorderRadius));
-
-    // Ensure padding does not exceed the limits
-    padding = Math.max(padding, minPadding);
-    padding = Math.min(padding, maxPadding);
-
-    return {
-      'padding-inline': `${padding}px`,
-    };
-  }
+  onClick = output<void>();
 
   /**
    * Get the spinner data
@@ -63,15 +28,5 @@ export class Button {
       size: 18,
       color: 'var(--primary-foreground)',
     };
-  }
-
-  /**
-   * Listen for keyboard events
-   */
-  @HostListener('keydown', ['$event']) onKeydownHandler(event: KeyboardEvent): void {
-    if (event.key === 'Enter' || event.key === ' ') {
-      this.button()?.nativeElement.click();
-      event.preventDefault();
-    }
   }
 }
