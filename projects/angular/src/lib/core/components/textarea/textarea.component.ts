61e880cab4847216af5375e3610f5a9e73f67224
molmos <molmosdev@gmail.com>
2024-10-28 00:09:10 +0100

    Refactor textarea component to update input handling and styles

--- projects/angular/src/lib/core/components/textarea/textarea.component.ts
+++ projects/angular/src/lib/core/components/textarea/textarea.component.ts
@@ -1,27 +1,20 @@
-import { Component, effect, input, InputSignal, model, ModelSignal, output, OutputEmitterRef } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, computed, input, InputSignal, model, ModelSignal, output, OutputEmitterRef } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'r-textarea',
   standalone: true,
-  imports: [FormsModule, NgClass],
+  imports: [NgClass],
   templateUrl: './textarea.component.html',
   animations: [],
 })
 export class Textarea {
   value: ModelSignal<string | null> = model<string | null>(null);
+  displayValue = computed(() => this.value() || '');
   label: InputSignal<string | undefined> = input<string | undefined>(undefined);
   error: InputSignal<boolean> = input<boolean>(false);
   hasValueForced: InputSignal<boolean> = input<boolean>(false);
   onChange: OutputEmitterRef<string | null> = output<string | null>();
-  inputValue: string = '';
-
-  constructor() {
-    effect(() => {
-      this.inputValue = this.value() || '';
-    });
-  }
 
   /**
    * Get the input trigger state
@@ -35,9 +28,10 @@ export class Textarea {
   /**
    * Update the value
    *
-   * @param {string} newValue
+   * @param {KeyboardEvent} event
    */
-  updateValue(newValue: string): void {
+  updateValue(event: KeyboardEvent): void {
+    const newValue = (event.target as HTMLInputElement).value;
     this.value.set(newValue === '' ? null : newValue);
     this.onChange.emit(this.value());
   }
