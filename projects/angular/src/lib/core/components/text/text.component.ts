eaf5f09bb4586518afbf1f171982545d13941f6d
molmos <molmosdev@gmail.com>
2024-10-28 00:08:02 +0100

    Refactor text component styles and update @realm-ui/angular package version to 0.0.0-beta.0.0.29

--- projects/angular/src/lib/core/components/text/text.component.ts
+++ projects/angular/src/lib/core/components/text/text.component.ts
@@ -1,5 +1,5 @@
-import { Component, effect, input, model, output } from '@angular/core';
-import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, computed, forwardRef, input, model, output } from '@angular/core';
+import { FormsModule, FormControl, ReactiveFormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NgClass } from '@angular/common';
 
 @Component({
@@ -7,25 +7,25 @@ import { NgClass } from '@angular/common';
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, NgClass],
   templateUrl: './text.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => Text),
+      multi: true,
+    },
+  ],
   animations: [],
 })
 export class Text {
   value = model<string | null>(null);
+  displayValue = computed(() => this.value() || '');
   label = input<string | undefined>(undefined);
   error = input<boolean>(false);
   type = input<string>('text');
   hasValueForced = input<boolean>(false);
   onChange = output<string | null>();
   clearable = input<boolean>(false);
-  inputValue: string = '';
-  blocked = input<boolean>(false);
-  formControl = input<FormControl | null>(null);
-
-  constructor() {
-    effect(() => {
-      this.inputValue = this.value() || '';
-    });
-  }
+  disabled = model<boolean>(false);
 
   /**
    * Get the input trigger state
@@ -41,12 +41,37 @@ export class Text {
    *
    * @param {string} newValue
    */
-  updateValue(newValue: string): void {
+  updateValue(event: KeyboardEvent): void {
+    const newValue = (event.target as HTMLInputElement).value;
     this.value.set(newValue === '' ? null : newValue);
     this.onChange.emit(this.value());
   }
 
   clear() {
-    this.updateValue('');
+    this.value.set(null);
+    this.onChange.emit(null);
+  }
+
+  /* ControlValueAccessor */
+
+  formControl = input<FormControl | null>(null);
+
+  onChangeFn: (value: string) => void = () => {};
+  onTouchedFn: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value.set(value);
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChangeFn = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouchedFn = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
   }
 }
