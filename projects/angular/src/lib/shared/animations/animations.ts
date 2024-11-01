662e7e3b5ef1e57d0fbedd5e372682448ccde1c2
molmos <molmosdev@gmail.com>
2024-11-01 17:25:21 +0100

    Remove unused animation triggers from shared animations

--- projects/angular/src/lib/shared/animations/animations.ts
+++ projects/angular/src/lib/shared/animations/animations.ts
@@ -10,20 +10,6 @@ export const fadeInOutZoomTrigger = trigger('fadeInOutZoomTrigger', [
   transition(':leave', [animate('0.1s', style({ opacity: 0, scale: 0.98 }))]),
 ]);
 
-export const fadeInOutVerticalTrigger = trigger('fadeInOutVerticalTrigger', [
-  transition(
-    ':enter',
-    [
-      style({ opacity: 0, transform: '{{ translateFrom }}' }),
-      animate('0.1s', style({ opacity: 1, transform: '{{ translateTo }}' })),
-    ],
-    { params: { translateFrom: 'translateY(-5px)', translateTo: 'translateY(0)' } }
-  ),
-  transition(':leave', [animate('0.1s', style({ opacity: 0, transform: '{{ translateFrom }}' }))], {
-    params: { translateFrom: 'translateY(-5px)', translateTo: 'translateY(0)' },
-  }),
-]);
-
 export const fadeInOutHorizontalTrigger = trigger('fadeInOutHorizontalTrigger', [
   transition(
     ':enter',
@@ -36,16 +22,4 @@ export const fadeInOutHorizontalTrigger = trigger('fadeInOutHorizontalTrigger', 
   transition(':leave', [animate('0.2s', style({ opacity: 0, transform: '{{ translateFrom }}' }))], {
     params: { translateFrom: 'translateX(-5px)', translateTo: 'translateX(0)' },
   }),
-]);
-
-export const incorrectBackgroundTrigger = trigger('incorrectBackgroundTrigger', [
-  state('true', style({ backgroundColor: '#FDE3E3FF' })),
-  state('false', style({ backgroundColor: 'transparent' })),
-  transition('* <=> *', [animate('0.1s')]),
-]);
-
-export const incorrectBackgroundGradientTrigger = trigger('incorrectBackgroundGradientTrigger', [
-  state('true', style({ background: '#FDE3E3FF' })),
-  state('false', style({ background: 'linear-gradient(90deg, transparent 0%, var(--background) 51%)' })),
-  transition('* <=> *', [animate('0.1s')]),
 ]);
