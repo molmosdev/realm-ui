8f2bef3964178f42466f968d98ac2c14eec00369
molmos <molmosdev@gmail.com>
2024-11-25 21:26:31 +0100

    Reorganize public API exports for improved structure and clarity

--- projects/angular/src/public-api.ts
+++ projects/angular/src/public-api.ts
@@ -2,61 +2,61 @@
  * Public API Surface of realm-ui@angular
  */
 
-/* Button*/
-export * from './lib/core/components/button/button.component';
+/* Animations */
+export * from './lib/shared/animations/animations';
 
-/* Spinner */
-export * from './lib/core/components/spinner/spinner.component';
+/* Button */
+export * from './lib/core/components/button/button.component';
 
 /* Dialog */
 export * from './lib/core/components/dialog/dialog.component';
 export * from './lib/core/components/dialog/components/dialog-title/dialog-title.component';
 
-/* Dropdown */
-export * from './lib/core/components/dropdown/dropdown.component';
-export * from './lib/core/components/dropdown/components/dropdown-content/dropdown-content.component';
-export * from './lib/core/components/dropdown/components/dropdown-content/components/dropdown-title/dropdown-title.component';
-export * from './lib/core/components/dropdown/components/dropdown-content/components/dropdown-item/dropdown-item.component';
-export * from './lib/core/components/dropdown/components/dropdown-trigger/dropdown-trigger.component';
-
 /* Directives */
 export * from './lib/shared/directives/lazy-content.directive';
 
-/* Text */
-export * from './lib/core/components/text/text.component';
+/* Dropdown */
+export * from './lib/core/components/dropdown/dropdown.component';
+export * from './lib/core/components/dropdown/components/dropdown-content/dropdown-content.component';
+export * from './lib/core/components/dropdown/components/dropdown-content/components/dropdown-item/dropdown-item.component';
+export * from './lib/core/components/dropdown/components/dropdown-content/components/dropdown-title/dropdown-title.component';
+export * from './lib/core/components/dropdown/components/dropdown-trigger/dropdown-trigger.component';
 
 /* Number */
 export * from './lib/core/components/number/number.component';
 
+/* Option */
+export * from './lib/shared/components/option/option.component';
+
+/* Password */
+export * from './lib/core/components/password/password.component';
+
+/* Search */
+export * from './lib/core/components/search/search.component';
+
+/* Select */
+export * from './lib/core/components/select/select.component';
+
+/* Spinner */
+export * from './lib/core/components/spinner/spinner.component';
+
 /* Switch */
 export * from './lib/core/components/switch/switch.component';
-
-/* Animations */
-export * from './lib/shared/animations/animations';
 
 /* Table */
 export * from './lib/core/components/table/table.component';
 export * from './lib/core/components/table/components/row/row.component';
 export * from './lib/core/components/table/components/row/components/row-item/row-item.component';
 
-/* Textarea */
-export * from './lib/core/components/textarea/textarea.component';
-
-/* Select */
-export * from './lib/core/components/select/select.component';
-
-/* Search */
-export * from './lib/core/components/search/search.component';
-
-/* Option */
-export * from './lib/shared/components/option/option.component';
-
 /* Tabs */
 export * from './lib/core/components/tabs/tabs.component';
 export * from './lib/core/components/tabs/components/tab/tab.component';
 
-/* Password */
-export * from './lib/core/components/password/password.component';
+/* Text */
+export * from './lib/core/components/text/text.component';
+
+/* Textarea */
+export * from './lib/core/components/textarea/textarea.component';
 
 /* User */
 export * from './lib/core/components/user/user.component';
